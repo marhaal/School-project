06efe5fbbfe3f0b57fff477140616d6cf31f1f97
marhaal <marie95@live.no>
2019-02-15 21:44:28 +0100

    request page, form for request

--- Webside/urls.py
+++ Webside/urls.py
@@ -3,5 +3,8 @@ from . import views
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', views.homepage, name='home')
+    path('', views.homepage, name='home'),
+    path('requests', views.requests, name='requests'),
+    path('post/<int:pk>/', views.requests_detail, name='requests_detail'),
+    path('requests/new/', views.requests_new, name='requests_new'),
 ]
--- Webside/views.py
+++ Webside/views.py
@@ -1,4 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
+from .forms import RequestsForm
+
 
 def homepage(request):
-    return render(request, 'home.html',{})
+    return render(request, 'webside/home.html',{})
+
+def requests(request):
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'webside/requests.html', {'posts': posts})
+
+def requests_detail(request, pk):
+    post= get_object_or_404(Post, pk=pk)
+    return render(request, 'webside/requests_detail.html', {'post': post})
+
+def requests_new(request):
+    if request.method == "POST":
+        form = RequestsForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('requests_detail', pk=post.pk)
+    else:
+        form = RequestsForm()
+    return render(request, 'webside/requests_edit.html', {'form': form})
