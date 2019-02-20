e16c1a44757b2a698775b79d6d47eee2c7f51d8f
Bjørn André Aaslund <BjornAndre@Bjrns-MacBook-Pro.local>
2019-02-20 23:13:43 +0100

    Changhed the logout site from admin to shareboi

--- Webside/urls.py
+++ Webside/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.requests_detail, name='requests_detail'),
     path('requests/new/', views.requests_new, name='requests_new'),
     url(r'^login/$', auth_views.LoginView.as_view(), {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': 'login'}, name='logout'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), {'template_name': 'logged_out.html'}, name='logout'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^account_activation_sent/$', views.account_activation_sent, name='account_activation_sent'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
