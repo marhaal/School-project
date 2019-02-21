60389e446680da9fff69fab45c04f216b72990be
marhaal <marie95@live.no>
2019-02-21 10:31:39 +0100

    Virtual environment removed from repository

--- shareboivenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/magdalenatran/Documents/Industriell økonomi og teknologiledelse/TDT4140 Programvareutvikling/gruppe-77/shareboivenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
