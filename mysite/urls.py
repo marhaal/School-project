d9795c40b875583d239f3dd9b3a1eee33878cb77
marhaal <marie95@live.no>
2019-02-24 22:26:54 +0100

    static file configuration

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Webside.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
