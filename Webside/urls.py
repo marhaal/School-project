07bd2ae2cee737a9befe0f0b38994798ccbd2f22
marhaal <marie95@live.no>
2019-02-24 22:49:55 +0100

    changed settings

--- Webside/urls.py
+++ Webside/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import url
 from . import views
 from django.views.generic.base import TemplateView
 from django.contrib.auth import views as auth_views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.homepage, name='home'),
